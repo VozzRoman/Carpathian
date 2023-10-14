ee1e5da03392e5d3dde4fb28a3f216835a5dfc09
VozzRoman <looper0500@ukr.net>
2023-10-14 19:56:57 +0300

    adding

---
+++ files/db.js
@@ -0,0 +1,326 @@
+export const upcoming  = [
+	{
+		id: 1,
+		image: ['img/hoverla_1.jpg', 'img/hoverla-tab_1.jpg', 'img/hoverla-desk_1.jpg'],
+		type: 'Ascent to Hoverla',
+		price: 'From UAH 4,499/person',
+	},
+	{
+		id: 2,
+		image: ['img/bulovel.jpg', 'img/bucovel-tab-1.jpg', 'img/bukovel-desk_1.jpg'],
+		type: 'Ski tour to Bukovel',
+		price: 'From UAH 7,499/person',
+	},
+	{
+		id: 3,
+		image: ['img/carpathians.jpg', 'img/carpatian-tab-1.jpg', 'img/carpatian-desk_1.jpg'],
+		type: 'Week in Carpathians',
+		price: 'From UAH 9,999/person',
+	},
+
+];
+
+
+export const users = [
+	{
+		id: 1,
+		name: "Ostap",
+		background: ["img/revie-mobile_1.jpg", "img/revie-tab_1.jpg", "img/revie-desk_1.jpg"],
+		ava: "../img/user-mobile_2.jpg",
+		body: "Good organization, everything went smoothly. Friendly guide Taras told interesting facts about the area and customs throughout the tour.",
+		image:'../img/back-face-pic_2.jpg'
+	},
+	{
+		id: 2,
+		name: "Maria",
+		ava: "img/user-mobile_1.png",
+		background: ["img/revie-mobile_1.jpg", "img/revie-tab_1.jpg",  "img/revie-desk_1.jpg"],
+		image:'../img/back-face-pic_1.jpg',
+		body: "It was really cool! We saw everything - sun, snow, rain, fog! It's very exciting and interesting, you won't regret it - it's worth going and seeing everything with your own eyes. Everything is organized at the highest level."
+	},
+	{
+		id: 3,
+		name: "Anita",
+		ava: "img/user-mobile_3.jpg",
+		image:'img/back-face-pic_3.jpg',
+		background: ["img/revie-mobile_1.jpg", "img/revie-tab_1.jpg",  "img/revie-desk_1.jpg"],
+		body: "Who is going to the mountains for the first time - this is exactly the tour and the people who organize everything. Guide Makar is interesting stories, selection of a route for a comfortable ascent, motivation and support."
+	},
+
+]
+
+export const gallery = [
+	{id: 12,
+		image: ['img/gallery-mobile_1.jpg', 'img/gallery-tablet_1.jpg', 'img/gallery-desk_7.jpg'],
+		
+	},
+	{id: 13,
+		image: ['img/gallery-mobile_2.jpg', 'img/gallery-tablet_2.jpg', 'img/gallery-desk_1.jpg'],
+		
+	},
+	{id: 14,
+		image: ['img/gallery-mobile_3.jpg', 'img/gallery-tablet_3.jpg', 'img/gallery-desk_3.jpg'],
+		
+	},
+	{id: 15,
+		image: ['img/gallery-mobile_4.jpg', 'img/gallery-tablet_4.jpg', 'img/gallery-desk_2.jpg'],
+		
+	},
+	{id: 16,
+		image: ['../img/gallery-mobile_5.jpg', '../img/gallery-tablet_5.jpg', '../img/gallery-desk_5.jpg'],
+		
+	},
+	{id: 17,
+		image: ['../img/gallery-mobile_6.jpg', '../img/gallery-tablet_6.jpg', '../img/gallery-desk_4.jpg'],
+		
+	},
+	{id: 18,
+		image: ['../img/gallery-mobile_7.jpg', '../img/gallery-tablet_7.jpg', '../img/gallery-desk_6.jpg'],
+		
+	},
+	
+
+]
+
+export const upcomingData = [
+	{
+		id:"1",
+		slider:[
+		{	id:"45",
+			name:"Ascent to Hoverla",
+			image:['../img/tour-slider_hoverla_1.jpg', '../img/tour-slider_hoverla-desk_1.jpg'],
+			data:{
+				day: '1 day',
+				one: 'Departure from Lviv',
+				tow: 'Manyava Hermitage',
+				three: 'Manyava waterfall',
+				foure: 'Arrival to Bukovel',
+				five: 'Check-in',
+				six: 'Dinner',
+				seven: 'Evening by the fireplace'
+			},
+			price: "4,99/person"
+			
+			
+		},
+		{	id:"46",
+		name:"Ascent to Hoverla",
+		image:['../img/tour-slider_hoverla_2.jpg', '../img/tour-slider_hoverla-desk_2.jpg'],
+		data:{
+			day: '2 day',
+			one: 'Breakfast',
+			tow: 'Ascent to Hoverla',
+			three: 'Dinner',
+			foure: 'Reacretion',
+			five: 'Sauna',
+			six: 'Dinner',
+			seven: ''
+		},
+		price: "4,99/person"
+		},
+		{	id:"47",
+	name:"Ascent to Hoverla",
+	image:['../img/tour-slider_hoverla_3.jpg', '../img/tour-slider_hoverla-desk_3.jpg'],
+	data:{
+		day: '3 day',
+		one: 'Breakfast',
+		tow: 'A walk around Bukovel',
+		three: 'Check out from the hotel',
+		foure: 'Yaremche',
+		five: 'Arrival in Lviv',
+		six: '',
+		seven: ''
+	},
+	price: "4,99/person"
+}
+		]
+	},
+{
+	id:'2',
+	slider: [
+		{	id:"49",
+name:"Ski tour to Bukovel",
+image:['../img/tour-slider-bukovel_1.jpg', '../img/tour-slider_bukove-desk_1.jpg'],
+data:{
+	day: '1 day',
+	one: 'Departure from Lviv',
+	tow: 'Manyava Hermitage',
+	three: 'Manyava waterfall',
+	foure: 'Arrival to Bukovel',
+	five: 'Check-in',
+	six: 'Dinner',
+	seven: 'Evening by the fireplace'
+},
+price: "7,99/person"
+
+},
+{	id:"50",
+name:"Ski tour to Bukovel",
+image:['../img/tour-slider-bukovel_2.jpg', '../img/tour-slider_bukove-desk_2.jpg'],
+data:{
+	day: '2 day',
+	one: 'Breakfast',
+	tow: 'Recreation',
+	three: 'Lunch',
+	foure: 'Recreation',
+	five: 'Check-in',
+	six: 'Dinner',
+	seven: 'Vacation'
+},
+price: "7,99/person"
+
+},
+{	id:"51",
+name:"Ski tour to Bukovel",
+image:[ '../img/tour-slider-bukovel_3.jpg', '../img/tour-slider_bukove-desk_3.jpg'],
+data:{
+	day: '3 day',
+	one: 'Breakfast',
+	tow: 'A walk around Bukovel',
+	three: 'Dinner',
+	foure: 'Recreation',
+	five: 'Sauna',
+	six: 'Dinner',
+	seven: ''
+},
+price: "7,99/person"
+
+},
+{	id:"52",
+name:"Ski tour to Bukovel",
+image:['../img/tour-slider-bukovel_4.jpg', '../img/tour-slider_bukove-desk_4.jpg'],
+data:{
+	day: '4 day',
+	one: 'Breakfast',
+	tow: 'Recreation',
+	three: 'Dinner',
+	foure: 'Departure from Bukovel',
+	five: 'Arrival in Lviv',
+	six: '',
+	seven: ''
+},
+price: "7,99/person"
+
+}
+	]
+},
+{
+	id:'3',
+	slider: [
+		{	id:"51",
+name:"Week in Carpathians",
+image:['../img/tour-slider-carpathian._1.jpg' ,'../img/tour-slider_carpathian-desk_1.jpg'],
+data:{
+	day: '1 day',
+	one: 'Departure from Lviv',
+	tow: 'Dovbush rocks',
+	three: 'Lunch',
+	foure: 'Tustan fortress',
+	five: 'Check-in',
+	six: 'Dinner',
+	seven: ''
+},
+price: "UAH 9,999/person"
+
+},
+{	id:"52",
+name:"Week in Carpathians",
+image:['../img/tour-slider-carpathian._2.jpg', '../img/tour-slider_carpathian-desk_2.jpg'],
+data:{
+	day: '2 day',
+	one: 'Breakfast',
+	tow: 'Lake Synevyr',
+	three: 'Ecopark “Valley of Wolves',
+	foure: 'Lunch',
+	five: 'Excursion to Kolochava',
+	six: 'Dinner',
+	seven: ''
+},
+price: "UAH 9,999/person"
+
+},
+{	id:"53",
+name:"Week in Carpathians",
+image:['../img/tour-slider-carpathian._3.jpg', '../img/tour-slider_carpathian-desk_3.jpg'],
+data:{
+	day: '3 day',
+	one: 'Breakfast',
+	tow: 'Shipit waterfall',
+	three: 'Gimba mountain',
+	foure: 'Recreation',
+	five: 'Dinner',
+	six: '',
+	seven: ''
+},
+price: "UAH 9,999/person"
+
+},
+{	id:"54",
+name:"Week in Carpathians",
+image:['../img/tour-slider-carpathian._4.jpg', '../img/tour-slider_carpathian-desk_4.jpg'],
+data:{
+	day: '4 day',
+	one: 'Breakfast',
+	tow: 'Deer farm in Iza',
+	three: 'Warm waters in Velyatino',
+	foure: 'Geographical center of Europe',
+	five: 'Dinner',
+	six: '',
+	seven: ''
+},
+price: "UAH 9,999/person"
+
+},
+{	id:"55",
+name:"Week in Carpathians",
+image:['../img/tour-slider-carpathian._5.jpg', '../img/tour-slider_carpathian-desk_5.jpg'],
+data:{
+	day: '5 day',
+	one: 'Breakfast',
+	tow: 'Ascent to Hoverla',
+	three: 'Reacreation',
+	foure: 'Dinner',
+	five: '',
+	six: '',
+	seven: ''
+},
+price: "UAH 9,999/person"
+
+},
+{	id:"56",
+name:"Week in Carpathians",
+image:['../img/tour-slider-carpathian._6.jpg', '../img/tour-slider_carpathian-desk_6.jpg'],
+data:{
+	day: '6 day',
+	one: 'Breakfast',
+	tow: 'Hut-museum from the "Shadows of Forgotten Ancestors"',
+	three: 'Lunch',
+	foure: 'Rafting of free time',
+	five: 'Dinner',
+	six: '',
+	seven: ''
+},
+price: "UAH 9,999/person"
+
+},
+{	id:"57",
+name:"Week in Carpathians",
+image:['../img/tour-slider-carpathian._7.jpg', '../img/tour-slider_carpathian-desk_7.jpg'],
+data:{
+	day: '7 day',
+	one: 'Breakfast',
+	tow: 'Dovbush trail',
+	three: 'Souvenir fair',
+	foure: 'Lunch',
+	five: 'Probij waterfall',
+	six: 'Return to Lviv',
+	seven: ''
+},
+price: "UAH 9,999/person"
+
+}
+	]
+}
+
+
+]
