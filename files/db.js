3ae4a41044f203a347ce0d14e271c39037742598
VozzRoman <looper0500@ukr.net>
2023-10-15 19:48:43 +0300

    adding

--- files/db.js
+++ files/db.js
@@ -26,16 +26,16 @@ export const users = [
 		id: 1,
 		name: "Ostap",
 		background: ["img/revie-mobile_1.jpg", "img/revie-tab_1.jpg", "img/revie-desk_1.jpg"],
-		ava: "../img/user-mobile_2.jpg",
+		ava: "img/user-mobile_2.jpg",
 		body: "Good organization, everything went smoothly. Friendly guide Taras told interesting facts about the area and customs throughout the tour.",
-		image:'../img/back-face-pic_2.jpg'
+		image:'img/back-face-pic_2.jpg'
 	},
 	{
 		id: 2,
 		name: "Maria",
 		ava: "img/user-mobile_1.png",
 		background: ["img/revie-mobile_1.jpg", "img/revie-tab_1.jpg",  "img/revie-desk_1.jpg"],
-		image:'../img/back-face-pic_1.jpg',
+		image:'img/back-face-pic_1.jpg',
 		body: "It was really cool! We saw everything - sun, snow, rain, fog! It's very exciting and interesting, you won't regret it - it's worth going and seeing everything with your own eyes. Everything is organized at the highest level."
 	},
 	{
@@ -67,15 +67,15 @@ export const gallery = [
 		
 	},
 	{id: 16,
-		image: ['../img/gallery-mobile_5.jpg', '../img/gallery-tablet_5.jpg', '../img/gallery-desk_5.jpg'],
+		image: ['img/gallery-mobile_5.jpg', 'img/gallery-tablet_5.jpg', 'img/gallery-desk_5.jpg'],
 		
 	},
 	{id: 17,
-		image: ['../img/gallery-mobile_6.jpg', '../img/gallery-tablet_6.jpg', '../img/gallery-desk_4.jpg'],
+		image: ['img/gallery-mobile_6.jpg', 'img/gallery-tablet_6.jpg', 'img/gallery-desk_4.jpg'],
 		
 	},
 	{id: 18,
-		image: ['../img/gallery-mobile_7.jpg', '../img/gallery-tablet_7.jpg', '../img/gallery-desk_6.jpg'],
+		image: ['img/gallery-mobile_7.jpg', 'img/gallery-tablet_7.jpg', 'img/gallery-desk_6.jpg'],
 		
 	},
 	
@@ -88,7 +88,7 @@ export const upcomingData = [
 		slider:[
 		{	id:"45",
 			name:"Ascent to Hoverla",
-			image:['../img/tour-slider_hoverla_1.jpg', '../img/tour-slider_hoverla-desk_1.jpg'],
+			image:['img/tour-slider_hoverla_1.jpg', 'img/tour-slider_hoverla-desk_1.jpg'],
 			data:{
 				day: '1 day',
 				one: 'Departure from Lviv',
@@ -105,7 +105,7 @@ export const upcomingData = [
 		},
 		{	id:"46",
 		name:"Ascent to Hoverla",
-		image:['../img/tour-slider_hoverla_2.jpg', '../img/tour-slider_hoverla-desk_2.jpg'],
+		image:['img/tour-slider_hoverla_2.jpg', 'img/tour-slider_hoverla-desk_2.jpg'],
 		data:{
 			day: '2 day',
 			one: 'Breakfast',
@@ -120,7 +120,7 @@ export const upcomingData = [
 		},
 		{	id:"47",
 	name:"Ascent to Hoverla",
-	image:['../img/tour-slider_hoverla_3.jpg', '../img/tour-slider_hoverla-desk_3.jpg'],
+	image:['img/tour-slider_hoverla_3.jpg', 'img/tour-slider_hoverla-desk_3.jpg'],
 	data:{
 		day: '3 day',
 		one: 'Breakfast',
@@ -140,7 +140,7 @@ export const upcomingData = [
 	slider: [
 		{	id:"49",
 name:"Ski tour to Bukovel",
-image:['../img/tour-slider-bukovel_1.jpg', '../img/tour-slider_bukove-desk_1.jpg'],
+image:['img/tour-slider-bukovel_1.jpg', 'img/tour-slider_bukove-desk_1.jpg'],
 data:{
 	day: '1 day',
 	one: 'Departure from Lviv',
@@ -156,7 +156,7 @@ price: "7,99/person"
 },
 {	id:"50",
 name:"Ski tour to Bukovel",
-image:['../img/tour-slider-bukovel_2.jpg', '../img/tour-slider_bukove-desk_2.jpg'],
+image:['img/tour-slider-bukovel_2.jpg', 'img/tour-slider_bukove-desk_2.jpg'],
 data:{
 	day: '2 day',
 	one: 'Breakfast',
@@ -172,7 +172,7 @@ price: "7,99/person"
 },
 {	id:"51",
 name:"Ski tour to Bukovel",
-image:[ '../img/tour-slider-bukovel_3.jpg', '../img/tour-slider_bukove-desk_3.jpg'],
+image:[ 'img/tour-slider-bukovel_3.jpg', 'img/tour-slider_bukove-desk_3.jpg'],
 data:{
 	day: '3 day',
 	one: 'Breakfast',
@@ -188,7 +188,7 @@ price: "7,99/person"
 },
 {	id:"52",
 name:"Ski tour to Bukovel",
-image:['../img/tour-slider-bukovel_4.jpg', '../img/tour-slider_bukove-desk_4.jpg'],
+image:['img/tour-slider-bukovel_4.jpg', 'img/tour-slider_bukove-desk_4.jpg'],
 data:{
 	day: '4 day',
 	one: 'Breakfast',
@@ -209,7 +209,7 @@ price: "7,99/person"
 	slider: [
 		{	id:"51",
 name:"Week in Carpathians",
-image:['../img/tour-slider-carpathian._1.jpg' ,'../img/tour-slider_carpathian-desk_1.jpg'],
+image:['img/tour-slider-carpathian._1.jpg' ,'img/tour-slider_carpathian-desk_1.jpg'],
 data:{
 	day: '1 day',
 	one: 'Departure from Lviv',
@@ -225,7 +225,7 @@ price: "UAH 9,999/person"
 },
 {	id:"52",
 name:"Week in Carpathians",
-image:['../img/tour-slider-carpathian._2.jpg', '../img/tour-slider_carpathian-desk_2.jpg'],
+image:['img/tour-slider-carpathian._2.jpg', 'img/tour-slider_carpathian-desk_2.jpg'],
 data:{
 	day: '2 day',
 	one: 'Breakfast',
@@ -241,7 +241,7 @@ price: "UAH 9,999/person"
 },
 {	id:"53",
 name:"Week in Carpathians",
-image:['../img/tour-slider-carpathian._3.jpg', '../img/tour-slider_carpathian-desk_3.jpg'],
+image:['img/tour-slider-carpathian._3.jpg', 'img/tour-slider_carpathian-desk_3.jpg'],
 data:{
 	day: '3 day',
 	one: 'Breakfast',
@@ -257,7 +257,7 @@ price: "UAH 9,999/person"
 },
 {	id:"54",
 name:"Week in Carpathians",
-image:['../img/tour-slider-carpathian._4.jpg', '../img/tour-slider_carpathian-desk_4.jpg'],
+image:['img/tour-slider-carpathian._4.jpg', 'img/tour-slider_carpathian-desk_4.jpg'],
 data:{
 	day: '4 day',
 	one: 'Breakfast',
@@ -273,7 +273,7 @@ price: "UAH 9,999/person"
 },
 {	id:"55",
 name:"Week in Carpathians",
-image:['../img/tour-slider-carpathian._5.jpg', '../img/tour-slider_carpathian-desk_5.jpg'],
+image:['img/tour-slider-carpathian._5.jpg', 'img/tour-slider_carpathian-desk_5.jpg'],
 data:{
 	day: '5 day',
 	one: 'Breakfast',
@@ -289,7 +289,7 @@ price: "UAH 9,999/person"
 },
 {	id:"56",
 name:"Week in Carpathians",
-image:['../img/tour-slider-carpathian._6.jpg', '../img/tour-slider_carpathian-desk_6.jpg'],
+image:['img/tour-slider-carpathian._6.jpg', 'img/tour-slider_carpathian-desk_6.jpg'],
 data:{
 	day: '6 day',
 	one: 'Breakfast',
@@ -305,7 +305,7 @@ price: "UAH 9,999/person"
 },
 {	id:"57",
 name:"Week in Carpathians",
-image:['../img/tour-slider-carpathian._7.jpg', '../img/tour-slider_carpathian-desk_7.jpg'],
+image:['img/tour-slider-carpathian._7.jpg', 'img/tour-slider_carpathian-desk_7.jpg'],
 data:{
 	day: '7 day',
 	one: 'Breakfast',
